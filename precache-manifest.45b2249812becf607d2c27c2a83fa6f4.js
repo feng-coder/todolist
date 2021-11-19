330cc63c006831799024b6a6cb886d689f9edcf0
Jason <neineiyuan@gmail.com>
2021-11-18 23:04:01 -0500

    add remaining files

---
+++ precache-manifest.45b2249812becf607d2c27c2a83fa6f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "45a612da72149ac54139",
+    "url": "/static/css/main.2e372b6d.chunk.css"
+  },
+  {
+    "revision": "45a612da72149ac54139",
+    "url": "/static/js/main.45a612da.chunk.js"
+  },
+  {
+    "revision": "6a02d334723e77f6e9cc",
+    "url": "/static/css/1.f3ca46c4.chunk.css"
+  },
+  {
+    "revision": "6a02d334723e77f6e9cc",
+    "url": "/static/js/1.6a02d334.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "80c62272cc5a8fabc132a628be255078",
+    "url": "/index.html"
+  }
+];
